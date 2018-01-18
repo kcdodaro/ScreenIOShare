e364ddbe4faa9e9f0d8c02c7c287fcf413aba6c0
Kyle <kcdodaro@gmail.com>
2018-01-18 04:29:08 -0500

    Added custom exceptions and ability to get internal IP address from the networking class

--- ScreenIOShare/Networking.cs
+++ ScreenIOShare/Networking.cs
@@ -1,12 +1,43 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Drawing.Imaging;
 
 namespace ScreenIOShare
 {
     class Networking
     {
+        string getInternalIPAddress()
+        {
+            var hostName = Dns.GetHostEntry(Dns.GetHostName());
+            string InternalIP = null;
+
+            foreach (var ip in hostName.AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    InternalIP = ip.ToString();
+                    break;
+                }
+                else
+                {
+                    InternalIP = null;
+                    throw new InternalIPNotFound();
+                }
+            }
+
+            return InternalIP;
+        }
     }
 }
