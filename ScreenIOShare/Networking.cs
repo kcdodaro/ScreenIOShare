14669bc220df52f3c3df9954e87a0350cfe47e19
Kyle <kcdodaro@gmail.com>
2018-01-18 04:39:01 -0500

    Fixed critical getInternalIPAddress method error

--- ScreenIOShare/Networking.cs
+++ ScreenIOShare/Networking.cs
@@ -33,7 +33,6 @@ namespace ScreenIOShare
                 else
                 {
                     InternalIP = null;
-                    throw new InternalIPNotFound();
                 }
             }
 
